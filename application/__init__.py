42702d0915ee4473c24f0a62c17ffe0d9c21a8e2
Usman Irale <superirale@gmail.com>
2017-01-03 18:45:46 +0100

    remote origin added

--- application/__init__.py
+++ application/__init__.py
@@ -3,17 +3,13 @@ from flask_sqlalchemy import SQLAlchemy, Pagination
 from sqlalchemy import or_
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail, Message
-
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
-
 mail = Mail(app)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 
 
 # Load the views
